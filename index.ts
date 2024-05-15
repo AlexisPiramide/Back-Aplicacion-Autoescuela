e28884c6963149aba5738b3e1629a3b0dc18b20f
Alexis <220240@fppiramide.com>
2024-05-15 12:39:40 +0200

    d

--- index.ts
+++ index.ts
@@ -8,15 +8,9 @@ import routerUsuario from "./usuario/infraestructure/rest/usuario.rest";
 
 dotenv.config();
 const port = process.env.PORT;
-const allowedOrigins = ["http://"+process.env.HOST+":5173"];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
-
 
 const app = express();
 app.use(express.json());
-app.use(cors(options));
 
 //routers
 app.use("/api/pregunta", routerPreguntas);
